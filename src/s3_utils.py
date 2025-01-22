9c3687d083d444062a4bc2a2733fbc1977d27453
lganic <sidwirb@gmail.com>
2025-01-22 23:18:35 +0000

    added s3 utils

---
+++ src/s3_utils.py
@@ -0,0 +1,34 @@
+import boto3
+
+def get_files_by_extension(bucket_name, folder_path, file_extensions):
+    """
+    Scans an S3 folder and returns all files with specified extensions.
+
+    Args:
+        bucket_name (str): Name of the S3 bucket.
+        folder_path (str): Path to the folder in the S3 bucket.
+        file_extensions (list): List of file extensions to look for (e.g., [".txt", ".jpg"]).
+
+    Returns:
+        list: A list of file keys matching the specified extensions.
+    """
+    s3_client = boto3.client('s3')
+    matching_files = []
+
+    if not folder_path.endswith('/'):
+        folder_path += '/'
+
+    paginator = s3_client.get_paginator('list_objects_v2')
+    operation_parameters = {
+        'Bucket': bucket_name,
+        'Prefix': folder_path
+    }
+
+    for page in paginator.paginate(**operation_parameters):
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                key = obj['Key']
+                if any(key.endswith(ext) for ext in file_extensions):
+                    matching_files.append(key)
+
+    return matching_files
