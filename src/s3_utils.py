4cb53ee60dd09a06e26037ee45ae329c28aadee0
lganic <sidwirb@gmail.com>
2025-01-22 23:53:15 +0000

    refactored s3 utils into class

--- src/s3_utils.py
+++ src/s3_utils.py
@@ -1,37 +1,41 @@
 import boto3
+import os
 
-def get_files_by_extension(bucket_name, folder_path, file_extensions):
-    """
-    Scans an S3 folder and returns all files with specified extensions.
+class S3_Wrapper:
+    def __init__(self):
+        self.s3_client = boto3.client('s3')
 
-    Args:
-        bucket_name (str): Name of the S3 bucket.
-        folder_path (str): Path to the folder in the S3 bucket.
-        file_extensions (list): List of file extensions to look for (e.g., [".txt", ".jpg"]).
+    def get_files_by_extension(self,bucket_name, folder_path, file_extensions):
+        """
+        Scans an S3 folder and returns all files with specified extensions.
 
-    Returns:
-        list: A list of file keys matching the specified extensions.
-    """
-    s3_client = boto3.client('s3')
-    matching_files = []
+        Args:
+            bucket_name (str): Name of the S3 bucket.
+            folder_path (str): Path to the folder in the S3 bucket.
+            file_extensions (list): List of file extensions to look for (e.g., [".txt", ".jpg"]).
 
-    if not folder_path.endswith('/'):
-        folder_path += '/'
-    
-    while folder_path and folder_path[0] == '/':
-        folder_path = folder_path[1:]
+        Returns:
+            list: A list of file keys matching the specified extensions.
+        """
+        matching_files = []
 
-    paginator = s3_client.get_paginator('list_objects_v2')
-    operation_parameters = {
-        'Bucket': bucket_name,
-        'Prefix': folder_path
-    }
+        if not folder_path.endswith('/'):
+            folder_path += '/'
+        
+        while folder_path and folder_path[0] == '/':
+            folder_path = folder_path[1:]
 
-    for page in paginator.paginate(**operation_parameters):
-        if 'Contents' in page:
-            for obj in page['Contents']:
-                key = obj['Key']
-                if any(key.endswith(ext) for ext in file_extensions):
-                    matching_files.append(key)
+        paginator = self.s3_client.get_paginator('list_objects_v2')
+        operation_parameters = {
+            'Bucket': bucket_name,
+            'Prefix': folder_path
+        }
 
-    return matching_files
+        for page in paginator.paginate(**operation_parameters):
+            if 'Contents' in page:
+                for obj in page['Contents']:
+                    key = obj['Key']
+                    if any(key.endswith(ext) for ext in file_extensions):
+                        matching_files.append(key)
+
+        return matching_files
